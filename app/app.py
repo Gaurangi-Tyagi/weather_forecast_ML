acd7717004535d54ee19881fe9f77c3658957f5c
Gaurangi Tyagi <gaurangi12tyagi@gmail.com>
2025-07-21 13:35:33 +0200

    lazy loading

--- app/app.py
+++ app/app.py
@@ -16,14 +16,6 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
 # Load models and encoders
-stage1_model = joblib.load(os.path.join(MODELS_DIR, "stage1_model.pkl"))
-regressor = joblib.load(os.path.join(MODELS_DIR, "regressor.pkl"))
-clf_desc = joblib.load(os.path.join(MODELS_DIR, "description_classifier.pkl"))
-clf_icon = joblib.load(os.path.join(MODELS_DIR, "icon_classifier.pkl"))
-scaler = joblib.load(os.path.join(MODELS_DIR, "scaler.pkl"))
-le_desc = joblib.load(os.path.join(MODELS_DIR, "label_encoder_description.pkl"))
-le_icon = joblib.load(os.path.join(MODELS_DIR, "label_encoder_icon.pkl"))
-city_encoder = joblib.load(os.path.join(MODELS_DIR, "stage1_city_encoder.pkl"))
 
 target_columns = [
     "tempmax",
@@ -60,7 +52,7 @@ regression_targets = [
     "sealevelpressure",
 ]
 
-city_columns = city_encoder.get_feature_names_out(["city"])
+# city_columns = city_encoder.get_feature_names_out(["city"])
 
 
 @app.route('/')
@@ -76,6 +68,15 @@ def predict():
         _type_: _description_
     """
     try:
+        stage1_model = joblib.load(os.path.join(MODELS_DIR, "stage1_model.pkl"))
+        regressor = joblib.load(os.path.join(MODELS_DIR, "regressor.pkl"))
+        clf_desc = joblib.load(os.path.join(MODELS_DIR, "description_classifier.pkl"))
+        clf_icon = joblib.load(os.path.join(MODELS_DIR, "icon_classifier.pkl"))
+        scaler = joblib.load(os.path.join(MODELS_DIR, "scaler.pkl"))
+        le_desc = joblib.load(os.path.join(MODELS_DIR, "label_encoder_description.pkl"))
+        le_icon = joblib.load(os.path.join(MODELS_DIR, "label_encoder_icon.pkl"))
+        city_encoder = joblib.load(os.path.join(MODELS_DIR, "stage1_city_encoder.pkl"))
+
         data = request.get_json()
         dt = datetime.strptime(data["datetime"], "%Y-%m-%d")
         year = dt.year
